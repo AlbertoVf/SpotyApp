df5a54e2de2f5c912770017faf357c6b972189e6
AlbertoVf <AlbertoVf97@protonmail.com>
2020-10-27 17:19:54 +0100

    Buscar artista

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class SearchComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class SearchComponent {
+  artists: any[] = [];
+  constructor(private spotify: SpotifyService) {}
+  buscar(artista: string): void {
+    console.log(artista);
+    this.spotify.getArtist(artista).subscribe((data: any) => {
+      this.artists = data.artists.items;
+      console.log(this.artists);
+    });
   }
-
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,20 +1,33 @@
+import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SpotifyService {
   constructor(private http: HttpClient) {}
+  private auth =
+    'Bearer BQD6tmj5P_KKegAKBS0kKwhRYKngl8oYLe31n78lWk4gbJzlfq5lxIyeHhYIAnbLv14C4FGu-IHMagzQfiM';
 
-  getNewReleases() {
+  getNewReleases(): Observable<any> {
     const headers = new HttpHeaders({
-      Authorization:
-        'Bearer BQAtnvgKf_fBUjqnd7hvbx2jP7kehIcqtQNjdJnZikznJU6xRutvKtah0Jb-h4R88wZEb3u95xiO8owjmHs',
+      Authorization: this.auth,
     });
     return this.http.get('https://api.spotify.com/v1/browse/new-releases', {
       headers,
     });
+  }
+
+  getArtist(artista: string): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: this.auth,
+    });
+    return this.http.get(
+      `https://api.spotify.com/v1/search?q=${artista}&type=artist&limit=15`,
+      {
+        headers,
+      }
+    );
   }
 }
