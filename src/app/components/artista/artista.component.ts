4c00af0717ca28e1979f95be7501ebaa39e4f7bf
AlbertoVf <AlbertoVf97@protonmail.com>
2020-10-28 18:09:46 +0100

    Captura de errores

--- src/app/components/artista/artista.component.ts
+++ src/app/components/artista/artista.component.ts
@@ -11,6 +11,9 @@ export class ArtistaComponent {
   artista: any = {};
   tracks: any = {};
   loading: boolean;
+  error = false;
+  aviso: string;
+  cod: string;
   constructor(private router: ActivatedRoute, private spotify: SpotifyService) {
     this.loading = true;
     this.router.params.subscribe((params) => {
@@ -21,17 +24,33 @@ export class ArtistaComponent {
 
   getArtist(id: string): void {
     this.loading = true;
-    this.spotify.getArtist(id).subscribe((artista) => {
-      this.artista = artista;
-      this.loading = false;
-    });
+    this.spotify.getArtist(id).subscribe(
+      (artista) => {
+        this.artista = artista;
+        this.loading = false;
+      },
+      (errorService: any) => {
+        this.loading = false;
+        this.error = true;
+        this.cod = errorService.error.error.status;
+        this.aviso = errorService.error.error.message;
+      }
+    );
   }
 
   getTopTraks(id: string): void {
     this.loading = true;
-    this.spotify.getTopTraks(id).subscribe((tracks) => {
-      this.tracks = tracks;
-      this.loading = false;
-    });
+    this.spotify.getTopTraks(id).subscribe(
+      (tracks) => {
+        this.tracks = tracks;
+        this.loading = false;
+      },
+      (errorService: any) => {
+        this.loading = false;
+        this.error = true;
+        this.cod = errorService.error.error.status;
+        this.aviso = errorService.error.error.message;
+      }
+    );
   }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,13 +8,23 @@ import { SpotifyService } from '../../services/spotify.service';
 export class HomeComponent implements OnInit {
   newRelease: any = [];
   loading: boolean;
-
+  error = false;
+  aviso: string;
+  cod: string;
   constructor(private spotify: SpotifyService) {
     this.loading = true;
-    this.spotify.getNewReleases().subscribe((data: any) => {
-      this.newRelease = data;
-      this.loading = false;
-    });
+    this.spotify.getNewReleases().subscribe(
+      (data: any) => {
+        this.newRelease = data;
+        this.loading = false;
+      },
+      (errorService: any) => {
+        this.loading = false;
+        this.error = true;
+        this.cod = errorService.error.error.status;
+        this.aviso = errorService.error.error.message;
+      }
+    );
   }
 
   ngOnInit(): void {}
